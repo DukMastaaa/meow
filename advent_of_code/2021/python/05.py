281437f20a988f04e9c49cfbc6d1189564cd746f
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2021-12-06 09:30:28 +1000

    better algorithm for day 5

--- advent_of_code/2021/python/05.py
+++ advent_of_code/2021/python/05.py
@@ -20,16 +20,6 @@ Diagonal = namedtuple("Diagonal", ["x1", "y1", "x2", "y2"])
 Line = Union[Horizontal, Vertical, Diagonal]
 
 
-
-def coords_to_line(x1: int, y1: int, x2: int, y2: int) -> Line:
-    if x1 == x2:
-        return Vertical(x1, y1, y2)
-    elif y1 == y2:
-        return Horizontal(y1, x1, x2)
-    else:
-        return Diagonal(x1, y1, x2, y2)
-
-
 def get_input() -> tuple[list[Line], list[Line], list[Line]]:
     horizontals = []
     verticals = []
@@ -37,14 +27,16 @@ def get_input() -> tuple[list[Line], list[Line], list[Line]]:
     with open("../input/05.txt", "r") as file:
         for input_line in file:
             input_line = input_line.strip()
-            x1, x2, y1, y2 = (int(n) for n in INPUT_PATTERN.match(input_line).groups())
-            match line := coords_to_line(x1, x2, y1, y2):
-                case Horizontal(_):
-                    horizontals.append(line)
-                case Vertical(_):
-                    verticals.append(line)
-                case Diagonal(_):
-                    diagonals.append(line)
+            
+            x1, y1, x2, y2 = (int(n) for n in INPUT_PATTERN.match(input_line).groups())
+
+            if x1 == x2:
+                verticals.append(Vertical(x1, y1, y2))
+            elif y1 == y2:
+                horizontals.append(Horizontal(y1, x1, x2))
+            else:
+                diagonals.append(Diagonal(x1, y1, x2, y2))
+
     return horizontals, verticals, diagonals
 
 
@@ -53,23 +45,19 @@ def pos_to_number(p: Position) -> int:
 
 
 # no overloading functions :(
-def horizontal_to_set(h: Horizontal) -> set[int]:
+def horizontal_to_range(h: Horizontal):
     initial = pos_to_number(Position(h.x1, h.y))
     final = pos_to_number(Position(h.x2, h.y))
-    return set(
-        range(min(initial, final), max(initial, final) + 1)
-    )
+    return range(min(initial, final), max(initial, final) + 1)
 
 
-def vertical_to_set(v: Vertical) -> set[int]:
+def vertical_to_range(v: Vertical):
     initial = pos_to_number(Position(v.x, v.y1))
     final = pos_to_number(Position(v.x, v.y2))
-    return set(
-        range(min(initial, final), max(initial, final) + 1, GRID_LENGTH)
-    )
+    return range(min(initial, final), max(initial, final) + 1, GRID_LENGTH)
 
 
-def diagonal_to_set(d: Diagonal) -> set[int]:
+def diagonal_to_range(d: Diagonal):
     initial = pos_to_number(Position(d.x1, d.y1))
     final = pos_to_number(Position(d.x2, d.y2))
     
@@ -77,21 +65,51 @@ def diagonal_to_set(d: Diagonal) -> set[int]:
     second = max(initial, final)
     
     # (first, second) must then define either SW or SE direction.
-    # this changes range argument, i'm guessing...
-    line_in_se_direction = second % GRID_LENGTH > first % GRID_LENGTH
-    return set(
-        range(
-            first,
-            second + 1,
-            GRID_LENGTH + 1 if line_in_se_direction else GRID_LENGTH - 1
-        )
+    line_is_in_se_direction = second % GRID_LENGTH > first % GRID_LENGTH
+    return range(
+        first,
+        second + 1,
+        GRID_LENGTH + 1 if line_is_in_se_direction else GRID_LENGTH - 1
     )
 
 
-def q1(data):
+def combined_solution(data, run_q2: bool):
+    counter = Counter()
+
+    horizontals, verticals, diagonals = data
+
+    h_ranges = (horizontal_to_range(h) for h in horizontals)
+    v_ranges = (vertical_to_range(v) for v in verticals)
+    if run_q2:
+        d_ranges = (diagonal_to_range(d) for d in diagonals)
+
+    considered_ranges = chain(h_ranges, v_ranges, d_ranges) if run_q2 else chain(h_ranges, v_ranges)
+
+    for r in considered_ranges:
+        counter.update(r)
+
+    return sum(counter[e] >= 2 for e in counter)
+
+
+if __name__ == "__main__":
+    q1data = get_input()
+    q2data = get_input()
+    print(combined_solution(q1data, False))  # q1
+    print(combined_solution(q1data, True))  # q2
+
+
+# old code
+
+
+def q1_old(data):
+    """
+    although it works, this is just too complicated and convoluted...
+    no reason to do it this way when just iterating through and counting
+    is faster.
+    """
     horizontals, verticals, _ = data
-    h_sets = [horizontal_to_set(h) for h in horizontals]
-    v_sets = [vertical_to_set(v) for v in verticals]
+    h_sets = [set(horizontal_to_range(h)) for h in horizontals]
+    v_sets = [set(vertical_to_range(v)) for v in verticals]
     counter = Counter()
 
     # compare within h and v, separately
@@ -109,35 +127,3 @@ def q1(data):
     counter.update(whole_intersection)
 
     return sum(counter[e] >= 1 for e in counter)
-
-
-def q2(data):
-    horizontals, verticals, diagonals = data
-    h_sets = [horizontal_to_set(h) for h in horizontals]
-    v_sets = [vertical_to_set(v) for v in verticals]
-    d_sets = [diagonal_to_set(d) for d in diagonals]
-    counter = Counter()
-
-    # compare within h, v, d, separately
-    for set1, set2 in chain(combinations(h_sets, 2), combinations(v_sets, 2), combinations(d_sets, 2)):
-        intersection = set1.intersection(set2)
-        counter.update(intersection)
-
-    # union h, v, d separately
-    union_h = reduce(set.union, h_sets)
-    union_v = reduce(set.union, v_sets)
-    union_d = reduce(set.union, d_sets)
-
-    # intersect h and v together, count elements???????????
-    # this should work because no diagonal lines, so lines are constrained
-    whole_intersection = union_h.intersection(union_v)
-    counter.update(whole_intersection)
-
-    return sum(counter[e] >= 1 for e in counter)
-
-
-
-if __name__ == "__main__":
-    q1data = get_input()
-    # q2data = get_input()
-    print(q1(q1data))
