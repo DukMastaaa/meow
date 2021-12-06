5861484697b24ff18fa41b232a2e1cb031120497
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2021-12-06 15:52:46 +1000

    day 6 aoc python solution

---
+++ advent_of_code/2021/python/06.py
@@ -0,0 +1,60 @@
+from collections import Counter
+
+def get_input():
+    with open("../input/06.txt", "r") as file:
+        data = [int(num) for num in file.readline().strip().split(",")]
+    return data
+
+
+def q(data, iterations: int):
+    c = Counter()
+    c.update(data)
+
+    for _ in range(iterations):
+        add_6s_and_8s_from_0 = c[0]
+        for i in range(1, 9):
+            c[i-1] = c[i]
+        c[8] = add_6s_and_8s_from_0
+        c[6] += add_6s_and_8s_from_0
+
+    return c.total()
+
+if __name__ == "__main__":
+    q1data = get_input()
+    print(q(q1data, 256))
+
+
+# old
+
+
+DEFAULT_LIFETIME = 6
+
+
+def q1(data):
+    data = [(n, DEFAULT_LIFETIME) for n in data]
+    for _ in range(80):
+        # print(",".join(list(str(t[0]) for t in data)))
+        indices_to_reduce = []  # contains index
+        fishes_to_reset = []    # contains index
+        fishes_born_today = []  # contains new lifetime
+        for index, fish in enumerate(data):
+            age, lifetime = fish
+            if age > 0:
+                indices_to_reduce.append(index)
+            else:
+                fishes_born_today.append(DEFAULT_LIFETIME + 2)
+                fishes_to_reset.append(index)
+        
+        for index in indices_to_reduce:
+            old_age, _ = data[index]
+            data[index] = (old_age - 1, DEFAULT_LIFETIME)
+        
+        for index in fishes_to_reset:
+            # _, lifetime = data[index]
+            # data[index] = (lifetime, lifetime)
+            data[index] = (DEFAULT_LIFETIME, DEFAULT_LIFETIME)
+
+        for lifetime in fishes_born_today:
+            data.append((DEFAULT_LIFETIME + 2, DEFAULT_LIFETIME))
+    
+    return len(data)
