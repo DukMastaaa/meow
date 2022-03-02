bf1afff4d4978c26d834229ee117db90e2f251a9
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2022-03-02 23:20:12 +1000

    refactor ocaml day 16, add utils

--- advent_of_code/2021/ocaml/day16.ml
+++ advent_of_code/2021/ocaml/day16.ml
@@ -1,81 +1,45 @@
 open Core
 
-type operation =
-  | Sum
-  | Product
-  | Minimum
-  | Maximum
-  | GreaterThan
-  | LessThan
-  | Equal
-
-type packet_contents =
-  | Literal of int
-  | Operator of operation * packet list
-and packet =
+type packet = 
   { version  : int;
     contents : packet_contents;
   }
+and packet_contents =
+  | Sum of packet list
+  | Product of packet list
+  | Minimum of packet list
+  | Literal of int
+  | Maximum of packet list
+  | GreaterThan of packet * packet
+  | LessThan of packet * packet
+  | Equal of packet * packet
 
-let operation_of_int = function
-  | 0 -> Sum
-  | 1 -> Product
-  | 2 -> Minimum
-  | 3 -> Maximum
-  | 5 -> GreaterThan
-  | 6 -> LessThan
-  | 7 -> Equal
-  | _ -> assert false
+let string_of_contents_type = function
+  | Sum _ -> "+"
+  | Product _ -> "*"
+  | Minimum _ -> "min"
+  | Maximum _ -> "max"
+  | Literal _ -> ""
+  | GreaterThan _ -> ">"
+  | LessThan _ -> "<"
+  | Equal _ -> "="
 
-let string_of_operation = function
-  | Sum -> "+"
-  | Product -> "*"
-  | Minimum -> "min"
-  | Maximum -> "max"
-  | GreaterThan -> ">"
-  | LessThan -> "<"
-  | Equal -> "="
-
-let binary_of_hex_map = [
-    '0', "0000";
-    '1', "0001";
-    '2', "0010";
-    '3', "0011";
-    '4', "0100";
-    '5', "0101";
-    '6', "0110";
-    '7', "0111";
-    '8', "1000";
-    '9', "1001";
-    'A', "1010";
-    'B', "1011";
-    'C', "1100";
-    'D', "1101";
-    'E', "1110";
-    'F', "1111";
-  ] |> Map.of_alist_exn (module Char)
-
-let binary_of_hex s =
-  String.to_list s
-  |> List.map ~f:(Map.find_exn binary_of_hex_map)
-  |> List.reduce_exn ~f:(^)
-
-let int_of_binary s = int_of_string ("0b" ^ s)
-
-let select_from_n s n =
-  String.sub s ~pos:n ~len:(String.length s - n)
+let list_from_contents = function
+  | Literal _ -> []
+  | Sum ps | Product ps | Minimum ps | Maximum ps -> ps
+  | GreaterThan (fst, snd) | LessThan (fst, snd) | Equal (fst, snd) -> [fst; snd]
 
 let rec parse_literal rest_of_string =
   let rec extract_groups s =
     let data = String.sub s ~pos:1 ~len:4 in
-    let everything_else = select_from_n s 5 in
+    let everything_else = Utils.select_str_from_n s 5 in
     if Char.(=) s.[0] '1' then
       let new_data, after_literal = extract_groups everything_else in
       (data ^ new_data, after_literal)
     else
       (data, everything_else) in
   let binary, after_literal = extract_groups rest_of_string in
-  Literal (int_of_binary binary), after_literal
+  Literal (Utils.int_of_binary binary), after_literal
 
 (* amt is either length or count, depending on length type id *)
 and traverse_sub_packets sub_packets s current_amt total_amt amt_increment_calc =
@@ -90,6 +54,23 @@ and traverse_sub_packets sub_packets s current_amt total_amt amt_increment_calc 
   else
     assert false
 
+and packet_contents_from_list type_id sub_packets =
+  match type_id with
+  | 0 -> Sum sub_packets
+  | 1 -> Product sub_packets
+  | 2 -> Minimum sub_packets
+  | 3 -> Maximum sub_packets
+  | _ ->
+    assert (List.length sub_packets = 2);
+    (* sub_packets are in reverse order due to :: *)
+    let fst = List.nth_exn sub_packets 1 in
+    let snd = List.hd_exn sub_packets in
+    match type_id with
+      | 5 -> GreaterThan (fst, snd)
+      | 6 -> LessThan (fst, snd)
+      | 7 -> Equal (fst, snd)
+      | _ -> assert false
+
 and parse_operator rest_of_string type_id =
   let length_type_id = String.get rest_of_string 0 in
   let bits_in_constant =
@@ -97,10 +78,10 @@ and parse_operator rest_of_string type_id =
       | '0' -> 15
       | '1' -> 11
       | _ -> assert false in
-  let constant = int_of_binary @@
+  let constant = Utils.int_of_binary @@
     String.sub rest_of_string ~pos:1 ~len:bits_in_constant in
   let after_constant =
-    select_from_n rest_of_string (1 + bits_in_constant) in
+    Utils.select_str_from_n rest_of_string (1 + bits_in_constant) in
   let amt_increment_calc =
     match length_type_id with
       | '0' -> fun s new_s -> String.length s - String.length new_s
@@ -108,12 +89,12 @@ and parse_operator rest_of_string type_id =
       | _ -> assert false in
   let sub_packets, after_sub_packets =
     traverse_sub_packets [] after_constant 0 constant amt_increment_calc in
-  Operator (operation_of_int type_id, sub_packets), after_sub_packets
+  packet_contents_from_list type_id sub_packets, after_sub_packets
 
 and packet_of_string s =
-  let version = int_of_binary (String.sub s ~pos:0 ~len:3) in
-  let type_id = int_of_binary (String.sub s ~pos:3 ~len:3) in
-  let rest_of_whole_string = select_from_n s 6 in
+  let version = Utils.int_of_binary (String.sub s ~pos:0 ~len:3) in
+  let type_id = Utils.int_of_binary (String.sub s ~pos:3 ~len:3) in
+  let rest_of_whole_string = Utils.select_str_from_n s 6 in
   let contents, after_packet =
     match type_id with
       | 4 -> parse_literal rest_of_whole_string
@@ -121,64 +102,51 @@ and packet_of_string s =
   {version; contents}, after_packet
 
 let rec eval p =
-  let map_eval_and_reduce l ~f =
+  let eval_and_reduce l ~f =
     List.map l ~f:eval |> List.reduce_exn ~f in
-  let compare_sub_packets sub_packets ~cmp =
-    (* sub_packets are in reverse order due to :: *)
-    match sub_packets with
-      | second :: first :: []
-        -> if cmp (eval first) (eval second) then 1 else 0
-      | _ -> assert false in
+  let compare (first, second) ~cmp =
+    if cmp (eval first) (eval second) then 1 else 0 in
   match p.contents with
     | Literal n -> n
-    | Operator (op, sub_packets) ->
-      match op with
-        | Sum         -> map_eval_and_reduce sub_packets ~f:(+)
-        | Product     -> map_eval_and_reduce sub_packets ~f:( * )
-        | Minimum     -> map_eval_and_reduce sub_packets ~f:(min)
-        | Maximum     -> map_eval_and_reduce sub_packets ~f:(max)
-        | GreaterThan -> compare_sub_packets sub_packets ~cmp:(>)
-        | LessThan    -> compare_sub_packets sub_packets ~cmp:(<)
-        | Equal       -> compare_sub_packets sub_packets ~cmp:(=)
+    | Sum ps -> eval_and_reduce ps ~f:(+)
+    | Product ps -> eval_and_reduce ps ~f:( * )
+    | Minimum ps -> eval_and_reduce ps ~f:(min)
+    | Maximum ps -> eval_and_reduce ps ~f:(max)
+    | GreaterThan (first, second) -> compare (first, second) ~cmp:(>)
+    | LessThan (first, second) -> compare (first, second) ~cmp:(<)
+    | Equal (first, second) -> compare (first, second) ~cmp:(=)
 
-let rec print_packet p ~print_versions =
-  match p.contents with
-    | Literal n -> 
-      if print_versions then
-        Printf.printf "([%d] %d" p.version n
-      else
-        Printf.printf "%d" n;
-    | Operator (op, sub_packets) ->
-      let op_string = string_of_operation op in
-      begin
-        if print_versions then 
-          Printf.printf "([%d] %s" p.version op_string
-        else 
-          Printf.printf "(%s" op_string;
-        List.iter sub_packets ~f:(fun sub_packet ->
-          Printf.printf " "; 
-          print_packet sub_packet ~print_versions;
-        );
-      end;
-  Printf.printf ")"
+let rec print_packet {version; contents} ~print_versions =
+  let packet_is_literal, literal_n = match contents with
+    | Literal n -> (true, n)
+    | _ -> (false, 0) in
+  if not packet_is_literal || print_versions
+    then Printf.printf "(";
+  if print_versions
+    then Printf.printf "[%d] " version;
+  Printf.printf "%s" (string_of_contents_type contents);
+  (* Literal _ gives [] *)
+  List.iter (list_from_contents contents) ~f:(fun sub_packet ->
+    Printf.printf " "; 
+    print_packet sub_packet ~print_versions;
+  );
+  if packet_is_literal then
+    Printf.printf "%d" literal_n
+  else Printf.printf ")"
 
 let q1 whole_packet =
-  let rec sum_of_versions sum {version; contents} =
-    match contents with
-      | Literal _
-        -> sum + version
-      | Operator (_, sub_packets) 
-        -> sum + version + List.fold sub_packets ~init:0 ~f:sum_of_versions
-    in
+  let rec sum_of_versions running_total {version; contents} =
+    let sub_packet_total = List.fold (list_from_contents contents) ~init:0 ~f:sum_of_versions in
+    running_total + version + sub_packet_total in
   sum_of_versions 0 whole_packet
 
 let q2 whole_packet = eval whole_packet
 
 let run filename =
   let hex = List.hd_exn (In_channel.read_lines filename) in
-  let binary = binary_of_hex hex in
+  let binary = Utils.binary_of_hex hex in
   let whole_packet, after_packet = packet_of_string binary in
-  assert (String.is_empty after_packet || int_of_binary after_packet = 0);
+  assert (String.is_empty after_packet || Utils.int_of_binary after_packet = 0);
   print_packet whole_packet ~print_versions:false;
   Printf.printf "\n";
   (q1 whole_packet, q2 whole_packet)
---
+++ advent_of_code/2021/ocaml/utils.ml
@@ -0,0 +1,30 @@
+open Core
+
+let binary_of_hex_map = [
+    '0', "0000";
+    '1', "0001";
+    '2', "0010";
+    '3', "0011";
+    '4', "0100";
+    '5', "0101";
+    '6', "0110";
+    '7', "0111";
+    '8', "1000";
+    '9', "1001";
+    'A', "1010";
+    'B', "1011";
+    'C', "1100";
+    'D', "1101";
+    'E', "1110";
+    'F', "1111";
+  ] |> Map.of_alist_exn (module Char)
+
+let binary_of_hex s =
+  String.to_list s
+  |> List.map ~f:(Map.find_exn binary_of_hex_map)
+  |> List.reduce_exn ~f:(^)
+
+let int_of_binary s = int_of_string ("0b" ^ s)
+
+let select_str_from_n s n =
+    String.sub s ~pos:n ~len:(String.length s - n)
