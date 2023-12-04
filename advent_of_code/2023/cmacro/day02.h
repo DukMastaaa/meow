74be6bc6e2db4272206eb9aeccaf910d7bdb404f
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2023-12-05 01:59:00 +1000

    c macro parsing works

--- advent_of_code/2023/cmacro/day02.h
+++ advent_of_code/2023/cmacro/day02.h
@@ -3,6 +3,7 @@
 #define CLOSE )
 #define LITOPEN (
 #define LITCLOSE )
+#define LITCOMMA ,
 
 #define EVAL0(x) x
 #define EVAL1(x) EVAL0(EVAL0(x))
@@ -18,6 +19,7 @@
 #define CAT_(x, ...) x##__VA_ARGS__
 
 #define APPLY(F, ...) F(__VA_ARGS__)
+#define DEFERAPPLY(F, ...) F BLANK (__VA_ARGS__)
 #define DEFERNOPAREN(F, x) F BLANK x
 
 // #define CONSUME_GAME(...) CONSUME_GAME_##__VA_ARGS__
@@ -29,51 +31,67 @@
 // #define MACHINE(...) EVAL0(CAT(PARSE_GAME_NUMBER_, CONSUME_GAME(__VA_ARGS__)) CLOSE)
 
 #define Game LITOPEN
-#define COLON LITCLOSE LITOPEN LITOPEN PARSEBODY OPEN
-#define PARSEBODY(x) [x]
-#define EOL EOL_DUMMY CLOSE LITCLOSE LITCLOSE
+#define COLON LITCOMMA LITOPEN FOO OPEN
+#define EOL CLOSE LITCLOSE LITCLOSE
+#define COMMA CLOSE FOO OPEN
+#define SEMICOLON CLOSE LITCLOSE LITOPEN FOO OPEN
+
+
+
+// #define GET_COLOUR(tokens) CAT(GET_COLOUR_, tokens)
+// #define GET_COLOUR_red red CLOSE
+// #define GET_COLOUR_green green CLOSE
+// #define GET_COLOUR_blue blue CLOSE
+// GET_COLOUR(blue beans blah blah)
+
+
 
 // goal: parse "3 blue" into (0,0,3), "4 red" into (4,0,0), "2 green" into (0,2,0)
 
 
-#define WORDSPLIT(tokens) XEVAL0(CAT(GET_NUMBER_, tokens CLOSE ) ) CLOSE
-#define GET_NUMBER_1 1 , GET_COLOUR OPEN
-#define GET_NUMBER_2 2 , GET_COLOUR OPEN
-#define GET_NUMBER_3 3 , GET_COLOUR OPEN
-
-#define GET_COLOUR(tokens) CAT(GET_COLOUR_, tokens)
-#define GET_COLOUR_red red CLOSE
-#define GET_COLOUR_green green CLOSE
-#define GET_COLOUR_blue blue CLOSE
-// GET_COLOUR(blue beans blah blah)
-
-// WORDSPLIT(3 blue 2 red)
+// #define WORDSPLIT(tokens) EVAL0(CAT(COMMA_AFTER_NUMBER_, tokens))
+#define WORDSPLIT(tokens) CAT(COMMA_AFTER_NUMBER_, tokens)
+#define COMMA_AFTER_NUMBER_0 0 LITCOMMA
+#define COMMA_AFTER_NUMBER_1 1 LITCOMMA
+#define COMMA_AFTER_NUMBER_2 2 LITCOMMA
+#define COMMA_AFTER_NUMBER_3 3 LITCOMMA
+#define COMMA_AFTER_NUMBER_4 4 LITCOMMA
+#define COMMA_AFTER_NUMBER_5 5 LITCOMMA
+#define COMMA_AFTER_NUMBER_6 6 LITCOMMA
+#define COMMA_AFTER_NUMBER_7 7 LITCOMMA
+#define COMMA_AFTER_NUMBER_8 8 LITCOMMA
+#define COMMA_AFTER_NUMBER_9 9 LITCOMMA
 
 // WORDSPLIT(2 red)
 
-#define WORDCOMBO(number, colour)     WORDCOMBO_##colour OPEN number CLOSE    hmm OPEN    WORDCOMBO_ALT BLANK OPEN WORDSPLIT OPEN
-#define WORDCOMBO_ALT(number, colour) WORDCOMBO_##colour OPEN number CLOSE    hmm OPEN    WORDCOMBO BLANK OPEN WORDSPLIT OPEN
-#define WORDCOMBO_red(number) LITOPEN number , 0 , 0 LITCLOSE
-#define WORDCOMBO_green(number) LITOPEN 0 , number , 0 LITCLOSE
-#define WORDCOMBO_blue(number) LITOPEN 0 , 0 , number LITCLOSE
+#define WORDCOMBO(number, colour) WORDCOMBO_##colour OPEN number CLOSE
+#define WORDCOMBO_red(number) LITOPEN number LITCOMMA 0 LITCOMMA 0 LITCLOSE
+#define WORDCOMBO_green(number) LITOPEN 0 LITCOMMA number LITCOMMA 0 LITCLOSE
+#define WORDCOMBO_blue(number) LITOPEN 0 LITCOMMA 0 LITCOMMA number LITCLOSE
+
+// EVAL0(WORDCOMBO(3, blue))
 
 // EVAL1(WORDCOMBO(3, blue))
 // EVAL1(WORDCOMBO(4, red))
 // EVAL1(WORDCOMBO(2, green))
 
-// EVAL0(  WORDCOMBO BLANK OPEN WORDSPLIT(3 blue 2 red)  )
-// EVAL0( EVAL0(  WORDCOMBO OPEN WORDSPLIT (3 blue 2 red)  ) )
-// EVAL( EVAL0( EVAL0(  WORDCOMBO OPEN WORDSPLIT(3 blue 2 red)  ) ) )
+#define FOO(tokens) APPLY(WORDCOMBO, WORDSPLIT(tokens))
 
-// ( WORDCOMBO_ALT ( WORDSPLIT ( 2 red )
-// ( WORDCOMBO_ALT BLANK ( WORDSPLIT ( 2 red )
-//  ( ( WORDCOMBO_ALT BLANK ( WORDSPLIT ( 2 red ) )
-
-EVAL0 (OPEN DEFERNOPAREN(WORDCOMBO, OPEN WORDSPLIT (2 red) ))
-
-// ( WORDCOMBO_ALT ( 2 , red ) )
+// FOO(3 blue)
 
 
-// #define INPUT Game 1 COLON 3 blue COMMA 4 red SEMICOLON 1 red COMMA 2 green COMMA 6 blue SEMICOLON 2 green EOL
-// EVAL0(INPUT)
 
+#define INPUT Game 1 COLON 3 blue COMMA 4 red SEMICOLON 1 red COMMA 2 green COMMA 6 blue SEMICOLON 2 green EOL
+// #define INPUT Game 1 COLON 3 blue EOL
+INPUT
+EVAL0(INPUT)
+EVAL0(EVAL0(INPUT))
+// EVAL0(EVAL0(EVAL0(INPUT)))
+// EVAL0(EVAL0(EVAL0(EVAL0(INPUT))))
+
+
+
+
+// #define TEST(n, ...) n before TEST2(__VA_ARGS__) after
+// #define TEST2(x) meow x beans
+// TEST (3,((0,0,1)(0,1,0))((1,0,0)))
