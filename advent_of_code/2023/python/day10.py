02751527a9aa00ce9bd3d506f0da17f7d56cbb4c
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2023-12-11 16:01:30 +1000

    clean up day 10

--- advent_of_code/2023/python/day10.py
+++ advent_of_code/2023/python/day10.py
@@ -4,13 +4,15 @@ from enum import Enum
 from queue import SimpleQueue
 from typing import Union
 
+Pos = tuple[int, int]
+
 
 class Dir(Enum):
     NORTH = 0
     EAST = 1
     SOUTH = 2
     WEST = 3
-    
+
     def opposite(self) -> "Dir":
         match self:
             case Dir.NORTH:
@@ -36,7 +38,25 @@ class Dir(Enum):
             case Dir.WEST:
                 return Dir.NORTH
         raise ValueError
-    
+
+
+def pos_in_dir(pos: Pos, dir: Dir) -> Pos:
+    i, j = pos
+    match dir:
+        case Dir.NORTH:
+            return i-1, j
+        case Dir.EAST:
+            return i, j+1
+        case Dir.SOUTH:
+            return i+1, j
+        case Dir.WEST:
+            return i, j-1
+
+
+def pos_in_bounds(pos: Pos, height: int, width: int) -> bool:
+    return (0 <= pos[0] < height and 0 <= pos[1] < width)
+
+
 DIRS = (Dir.NORTH, Dir.EAST, Dir.SOUTH, Dir.WEST)
 
 MESHING_CHARS = [
@@ -46,11 +66,12 @@ MESHING_CHARS = [
 
 @dataclasses.dataclass
 class Tile:
-    pos: tuple[int, int] = None  # type: ignore
+    pos: Pos
+    char: str
     neighbours: list[Union["Tile", None]] = dataclasses.field(
         default_factory=lambda: [None, None, None, None]
     )
-    
+
     def connect(self, other: "Tile", dir: Dir, l: list[str]):
         self.neighbours[dir.value] = other
         backlink = False
@@ -61,106 +82,65 @@ class Tile:
                 backlink = True
         except IndexError:
             pass
-        # print(f"{dir.name}, {(i, j)} {'<->' if backlink else '-->'} {neighbour_pos}")
 
 
-def pos_in_dir(pos: tuple[int, int], dir: Dir) -> tuple[int, int]:
-        i, j = pos
-        match dir:
-            case Dir.NORTH:
-                return i-1, j
-            case Dir.EAST:
-                return i, j+1
-            case Dir.SOUTH:
-                return i+1, j
-            case Dir.WEST:
-                return i, j-1
+def parse() -> tuple[dict[Pos, Tile], Pos, int, int]:
+    with open("../input/10.txt", "r") as file:
+        l = [s.strip() for s in file.read().strip().split('\n')]
+    d = {(i, j): Tile((i, j), c) for i, line in enumerate(l)
+         for j, c in enumerate(line)}
+    height = len(l)
+    width = len(l[0])
+    starting_pos = (0, 0)
+    # this constructs the whole graph which i don't need lol
+    # oh well i'll keep it in
+    for i, line in enumerate(l):
+        for j, c in enumerate(line):
+            if c == "S":
+                starting_pos = (i, j)
+                continue
+            for dir in DIRS:
+                if c in MESHING_CHARS[dir.value]:
+                    try:
+                        d[(i, j)].connect(d[pos_in_dir((i, j), dir)], dir, l)
+                    except KeyError:
+                        pass
+    return d, starting_pos, height, width
 
 
-def pos_in_bounds(pos: tuple[int, int], height: int, width: int) -> bool:
-    return (0 <= pos[0] < height and 0 <= pos[1] < width)
+def get_loop(d: dict[Pos, Tile], starting_pos: Pos,
+             height: int, width: int) -> dict[Pos, int]:
+    # BFS
+    q: SimpleQueue[tuple[Pos, Pos, int]] = SimpleQueue()
+    distances = {}
+    q.put((starting_pos, starting_pos, 0))
+    while not q.empty():
+        pos, prev, dist = q.get()
+        if pos in distances:
+            break  # loop found
+        distances[pos] = dist
+        for neighbour in d[pos].neighbours:
+            if neighbour is not None and neighbour.pos != prev:
+                q.put((neighbour.pos, pos, dist+1))
+    return distances
 
 
-# with open("advent_of_code/2023/input/10test.txt", "r") as file:
-with open("../input/10.txt", "r") as file:
-    l = [s.strip() for s in file.read().strip().split('\n')]
-
-d: dict[tuple[int, int], Tile] = {}
-for i, line in enumerate(l):
-    for j, c in enumerate(line):
-        d[(i, j)] = Tile()
-        d[(i, j)].pos = (i, j)
-
-height = len(l)
-width = len(l[0])
-
-starting_pos = (0, 0)
-for i, line in enumerate(l):
-    for j, c in enumerate(line):
-        NEIGHBOURS = {
-            Dir.NORTH: (i-1, j),
-            Dir.EAST: (i, j+1),
-            Dir.SOUTH: (i+1, j),
-            Dir.WEST: (i, j-1),
-        }
-        for dir in DIRS:
-            if c in MESHING_CHARS[dir.value]:
-                try:
-                    d[(i, j)].connect(d[pos_in_dir((i, j), dir)], dir, l)
-                except KeyError:
-                    pass
-        if c == "S":
-            starting_pos = (i, j)
-
-
-q: SimpleQueue[tuple[tuple[int, int], tuple[int, int], int]] = SimpleQueue()
-distances = {}
-q.put((starting_pos, starting_pos, 0))
-while not q.empty():
-    pos, prev, dist = q.get()
-    if pos in distances:
-        break  # loop found?
-    distances[pos] = dist
-    for neighbour in d[pos].neighbours:
-        if neighbour is not None and neighbour.pos != prev:
-            q.put((neighbour.pos, pos, dist+1))
-
-q1 = max(distances.items(), key=lambda t: t[1])
-print(q1)
-
-
-
-# traverse the loop counterclockwise, then keep looking to the left.
-# if you see a tile that isn't a part of the loop, that whole region is inside the loop.
-# build up a set of regions (need map from pos to region), then at the end sum the contained tiles
-
-# rather than dealing with direction, can go to top-left tile in the loop then go right from there
-
-assert([len([t for t in d[pos].neighbours if t is not None]) == 2 for pos in distances])
-# lexicographic sort lol
-top_left_pos = min(distances.keys())
-assert d[top_left_pos].neighbours[Dir.EAST.value] is not None
-
-
-regions: list[set[tuple[int, int]]] = []
-pos_to_region: dict[tuple[int, int], set[tuple[int, int]]] = {}
-
-
-def explore_region(start_pos: tuple[int, int],
-                   loop: dict[tuple[int, int], int],
-                   pos_to_region: dict[tuple[int, int], set[tuple[int, int]]],
-                   regions: list[set[tuple[int, int]]],
+def explore_region(start_pos: Pos,
+                   loop: dict[Pos, int],
+                   pos_to_region: dict[Pos, set[Pos]],
+                   regions: list[set[Pos]],
                    height: int, width: int):
     if not pos_in_bounds(start_pos, height, width):
         print("out of bounds")
         return
     if start_pos in loop or start_pos in pos_to_region:
         return
-    q = []
-    q.append((start_pos, start_pos))
+    stack = []
+    stack.append((start_pos, start_pos))
     new_region = set()
-    while q:
-        pos, prev = q.pop()
+    # DFS
+    while stack:
+        pos, prev = stack.pop()
         if pos in loop:
             continue
         pos_to_region[pos] = new_region
@@ -168,63 +148,89 @@ def explore_region(start_pos: tuple[int, int],
         for adjacent_pos in map(lambda dir: pos_in_dir(pos, dir), DIRS):
             if (pos_in_bounds(adjacent_pos, height, width) and adjacent_pos != prev
                     and adjacent_pos not in loop and adjacent_pos not in pos_to_region):
-                q.append((adjacent_pos, pos))
+                stack.append((adjacent_pos, pos))
     if len(new_region) > 0:
         regions.append(new_region)
 
 
-explore = lambda pos: explore_region(
-    pos, distances, pos_to_region, regions, height, width
-)
+def find_inner_regions(d: dict[Pos, Tile], loop: dict[Pos, int],
+                       height: int, width: int) -> tuple[list[set[Pos]], dict[Pos, set[Pos]]]:
+    # assert that no branches come out of loop
+    assert ([len([t for t in d[pos].neighbours if t is not None])
+            == 2 for pos in loop])
+    # lexicographic sort lol
+    top_left_pos = min(loop.keys())
+    # assert that top left tile goes right
+    assert d[top_left_pos].neighbours[Dir.EAST.value] is not None
 
-prev = top_left_pos
-prev_dir = Dir.EAST
-curr = d[top_left_pos].neighbours[Dir.EAST.value].pos  # type: ignore
-while curr != top_left_pos:
-    next_dir, next = [
-        (i, t.pos)
-        for i, t in enumerate(d[curr].neighbours)
-        if t is not None and t.pos != prev
-    ][0]
-    next_dir = Dir(next_dir)
-    # we travel clockwise.
-    explore(pos_in_dir(curr, next_dir.inside_when_clockwise()))
-    explore(pos_in_dir(curr, prev_dir.inside_when_clockwise()))
-    # update
-    prev = curr
-    curr = next
-    prev_dir = next_dir
+    # autoformat :sob:
+    def explore(pos): return explore_region(
+        pos, loop, pos_to_region, regions, height, width
+    )
+    regions: list[set[Pos]] = []
+    pos_to_region: dict[Pos, set[Pos]] = {}
 
-q2 = sum(map(lambda s: len(s), regions))
-print(q2)
-# print(regions)
+    # initial conditions set up for clockwise traversal from top left tile in loop
+    prev = top_left_pos
+    prev_dir = Dir.EAST
+    curr = d[top_left_pos].neighbours[Dir.EAST.value].pos  # type: ignore
+    while curr != top_left_pos:
+        next_dir, next = [
+            (i, t.pos)
+            for i, t in enumerate(d[curr].neighbours)
+            if t is not None and t.pos != prev
+        ][0]
+        next_dir = Dir(next_dir)
+        # look inward to the centre of the loop
+        explore(pos_in_dir(curr, next_dir.inside_when_clockwise()))
+        explore(pos_in_dir(curr, prev_dir.inside_when_clockwise()))
+        # update
+        prev = curr
+        curr = next
+        prev_dir = next_dir
 
-
-TRANSLATOR = {
-    "|": "│",
-    "-": "─",
-    "F": "┌",
-    "7": "┐",
-    "L": "└",
-    "J": "┘",
-    "S": "S",
-    ".": "."
-}
-
-
-# ss = []    
-# for i in range(height):
-#     for j in range(width):
-#         if (i, j) in distances:
-#             ss.append(TRANSLATOR[l[i][j]])
-#             # ss.append(".")
-#         elif (i, j) in pos_to_region:
-#             ss.append("I")
-#         else:
-#             ss.append("O")
-#     ss.append("\n")
-# ss.append("\n")
+    return regions, pos_to_region
 
 
-# with open("dump.txt", "wb") as file:
-#     file.write("".join(ss).encode('utf8'))
+def q1(loop: dict[Pos, int]) -> int:
+    return max(loop.values())
+
+
+def q2(regions: list[set[Pos]]) -> int:
+    return sum(map(lambda s: len(s), regions))
+
+
+def visualise_q2(loop: dict[Pos, int], pos_to_region: dict[Pos, set],
+                 d: dict[Pos, Tile], height: int, width: int):
+    CHAR_TO_UNICODE = {
+        "|": "│",
+        "-": "─",
+        "F": "┌",
+        "7": "┐",
+        "L": "└",
+        "J": "┘",
+        "S": "S",
+        ".": "."
+    }
+    ss = []
+    for i in range(height):
+        for j in range(width):
+            if (i, j) in loop:
+                ss.append(CHAR_TO_UNICODE[d[(i, j)].char])
+            elif (i, j) in pos_to_region:
+                ss.append("I")
+            else:
+                ss.append("O")
+        ss.append("\n")
+    ss.append("\n")
+    with open("dump.txt", "wb") as file:
+        file.write("".join(ss).encode('utf8'))
+
+
+if __name__ == "__main__":
+    d, starting_pos, height, width = parse()
+    loop = get_loop(d, starting_pos, height, width)
+    regions, pos_to_region = find_inner_regions(d, loop, height, width)
+    print(q1(loop))
+    print(q2(regions))
+    # visualise_q2(loop, pos_to_region, d, height, width)
